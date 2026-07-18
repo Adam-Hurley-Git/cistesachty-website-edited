17e2b6c52a98b5c66e4e98ab8248039415cdd606
Adam-Hurley-Git <hurleyadam10@gmail.com>
2026-07-18 20:09:41 +0200

    Remove entity-name field from inspection forms

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1098,7 +1098,6 @@ function Contact() {
             <Field label="E-mail" type="email" placeholder="vas@email.cz" />
             <Field label="Město / lokalita" placeholder="Břeclav / vaše lokalita" />
             <Field label="Typ objektu" placeholder="SVJ / BD / správce" />
-            <Field label="Název SVJ / správce" placeholder="Název subjektu" />
             <Field label="Adresa domu" placeholder="Ulice, číslo domu" className="sm:col-span-2" />
           </div>
           <label className="mt-4 block">
